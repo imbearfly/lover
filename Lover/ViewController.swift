6bcc3fe255b39e8d368ce9e418ac6d8428093fc6
Fly Hsiung <imbearfly@efgpdigiwinde-macbook-air.local>
2016-07-27 17:47:26 +0800

    Initial Commit

---
+++ Lover/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lover
+//
+//  Created by Fly on 7/27/16.
+//  Copyright © 2016 Fly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
